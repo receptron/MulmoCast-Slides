7b108cdefcb422a12719941889e60daa0c2cb52f
Yasutaka Nishii <yasutaka.nishii@gmail.com>
2026-03-27 16:18:56 +0900

    Revert "feat: upgrade mulmocast-viewer to 0.2.4 and enable media aspect ratio"
    This reverts commit 1bce3c479ecb0892e9bc3fe4669051be9c1f8e5a.

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -565,7 +565,6 @@ function discardRecordings() {
             :init-page="currentPage"
             v-model:audio-lang="audioLang"
             v-model:text-lang="textLang"
-            media-aspect-ratio="video"
             @updated-page="onUpdatedPage"
           />
         </div>
